a6accb714c2526273815ac44b2b568644b5aaa62
Daniel Cataño Restrepo <dasniel199410@gmail.com>
2019-01-23 12:53:54 -0500

    Git commands added in order to make a comit and a push per file modified

--- scratch.sh
+++ scratch.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 test=$(find . -name "README*")
+count=0
 for i in $test; do	
 	echo $i
 	echo $i | rev | cut -d'/' -f1 | rev
-	echo "Test" >> "${i}"
+	#echo " " >> "${i}"	
+	#$(git add $i)
+	#$(git commit -m "Commit to readme $i")
+	count=$((count+1))
+	#$(git push)
 done
+echo $count
+log=$(git log)
+echo $log | cut -d'commit' -f1
 
