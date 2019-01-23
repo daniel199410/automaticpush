e7ea03f59c133ea2aca975e6e879b189924e80d5
Daniel Cataño <dcatano@cidenet.com.co>
2019-01-23 09:32:46 -0500

    Initial configuration

---
+++ src/main/java/com/dcatano/automaticpush/AutomaticpushApplication.java
@@ -0,0 +1,14 @@
+package com.dcatano.automaticpush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticpushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomaticpushApplication.class, args);
+    }
+
+}
+
